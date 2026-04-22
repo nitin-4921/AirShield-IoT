4d43a6df64fc14e7784f6ab093de57f3152bc77d
Vishesh Pandey <behappy9699@gmail.com>
2026-04-22 22:43:42 +0530

    .

--- backend/server.js
+++ backend/server.js
@@ -310,7 +310,7 @@ app.post('/predict', async (req, res) => {
 });
 
 
-const PORT = 3000;
-app.listen(PORT, () => {
-    console.log(`Backend server running on http://localhost:${PORT}`);
+const Port = 3000;
+app.listen(Port, () => {
+    console.log(`Backend server running on http://localhost:${Port}`);
 });
